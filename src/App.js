910133d322dc8ab2c191e866e6b0d546de5b3462
Limon714 <limon4u714@gmail.com>
2022-09-08 23:50:35 +0600

    hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Index1 from "./HOOK_USESTATE/Index1";
+import Index2 from "./HOOK_USESTATE/Index2";
+import './HOOK_USESTATE/style.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Index2/>
+    </>
   );
 }
 
