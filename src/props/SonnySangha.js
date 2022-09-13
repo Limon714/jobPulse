0c98063d867359c286417c72bed1863f7474d317
Abdul Khalaque <72975868+Limon714@users.noreply.github.com>
2022-09-13 15:27:47 +0600

    Update SonnySangha.js

--- src/props/SonnySangha.js
+++ src/props/SonnySangha.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SonnySangha = ({ name, hours }) => {
     return <div>
-        <h2>Nice YouTuber {name} I learn {hours}</h2>
+        <h2>Nice YouTuber {name} Today I learn {hours}</h2>
   </div>;
 };
 
